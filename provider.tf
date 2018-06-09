dda0d589af6a679a8439dab0f2a8885bf2dbef80
Tejas Pillai <tejaspillai@Tejas.local>
2018-06-09 18:19:18 +0200

    added  ec2 instance

--- provider.tf
+++ provider.tf
@@ -21,3 +21,11 @@ resource "aws_subnet" "subnets" {
     Name = "subnet-${count.index+1}"
   }
 }
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "HelloWorld"
+  }
